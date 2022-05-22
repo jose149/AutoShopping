af677a60e87cd2b9f4c2d5409dea8f54c46f02ec
Jose Crespi <crespi.valero.jose@gmail.com>
2022-05-22 18:10:30 +0200

    Front end set up

---
+++ teleshopping/src/components/NavigationBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="navigation-bar">
+    <img class="navigationbar__logo"
+    src="../assets/logo.png"
+    alt="Logo">
+    <ul class="navigationbar__links">
+      <li></li>
+    </ul>
+    <div class="navigationbar__contact">CONTACT</div>
+
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'NavigationBar'
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  .navigation-bar{
+    height: 100px;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    &__logo{
+      height: 100%;
+    }
+
+  }
+</style>
