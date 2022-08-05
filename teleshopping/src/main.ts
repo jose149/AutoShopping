e51dc6e2cf879173663d48193c1cf94d0feef337
Jose Crespi <crespi.valero.jose@gmail.com>
2022-08-05 08:57:02 +0200

    clean code

--- teleshopping/src/main.ts
+++ teleshopping/src/main.ts
@@ -2,8 +2,6 @@ import { createApp } from 'vue'
 import App from './TeleShopping.vue'
 import { teleShoppingRouter } from './router/TeleShoppingRouter'
 
-
-
 const app = createApp(App)
 app.use(teleShoppingRouter)
 app.mount('#app')
